af6ad4a5cbe3f24a6c1900ad31a5eab00a0e4aeb
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-03-02 19:21:20 -0300

    req4e5 validação de email e senha, salva localStorage, obs: falta testes

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import LoginContext from '../context/LoginContext';
 
 export default function Login() {
+  const { email, setEmail, senha, setSenha } = useContext(LoginContext);
+
+  const handleEmail = (valueEmail) => {
+    setEmail(valueEmail);
+  };
+  const handleSenha = (valueSenha) => {
+    setSenha(valueSenha);
+  };
+  const handleClick = () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
+
+  const disableButton = () => {
+    const lengthinput = 6;
+    const regexValidation = /^([a-z]){1,}([a-z0-9._-]){1,}([@]){1}([a-z]){2,}([.]){1}([a-z]){2,}([.]?){1}([a-z]?){2,}$/i;
+    const validation = regexValidation.test(email)
+      && senha.length > lengthinput;
+    return !validation;
+  };
+
   return (
     <>
       <h2>LOGIN</h2>
@@ -10,6 +31,7 @@ export default function Login() {
         id="email"
         data-testid="email-input"
         placeholder="Email"
+        onChange={ (e) => { handleEmail(e.target.value); } }
       />
       <br />
       <input
@@ -17,11 +39,14 @@ export default function Login() {
         id="password"
         data-testid="password-input"
         placeholder="Password"
+        onChange={ (e) => { handleSenha(e.target.value); } }
       />
       <br />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ disableButton() }
+        onClick={ handleClick }
       >
         Enter
 
