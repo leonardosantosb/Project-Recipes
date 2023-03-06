d12c25e69d11a67e1eeeb748f317f44a1d62cde0
Amanda Zanata <amandazanata46@gmail.com>
2023-03-06 18:00:51 -0300

    pagina DoneRecipes 90%

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default function DoneRecipes() {
+  const [clickBtn, setClickBtn] = useState('all');
+  const getRecipes = JSON.parse(localStorage.getItem('doneRecipes')); // pega item no localStorage
+
+  const showCard = () => { // função com condição de colocar ou não a receita que foi marcada como 'checkada'
+    if (getRecipes === null || getRecipes.length < 1) { // se igua a null ou menor que 1 retorna mensagem
+      return <span>Termine alguma receita</span>;
+    }
+    if (clickBtn === 'all') { // faz o map para pegar o card da receita
+      return (getRecipes.map((meal, index) => (
+        // card da receita
+      )));
+    }
+    return (getRecipes.filter((meal) => meal.type === clickBtn).map((meal, index) => ( // faz um filter para mostrar o card da receita que foi realizada
+      // card da receita
+    )));
+  }
+
   return (
     <>
       <h1 data-testid="page-title">Done Recipes</h1>
@@ -10,25 +27,29 @@ export default function DoneRecipes() {
         <button
           type="button"
           data-testid="filter-by-all-btn" // data-testid solicitado no read-me
-          onClick={}
+          onClick={ () => setClickBtn('all') } // usa o click do estado para verificar o recebimento
         >
           All
         </button>
         <button
           type="button"
           data-testid="filter-by-meal-btn" // data-testid solicitado no read-me
-          onClick={}
+          onClick={ () => setClickBtn('meal') } // usa o click do estado para verificar o recebimento
         >
           Meals
         </button>
         <button
           type="button"
           data-testid="filter-by-drink-btn" // data-testid solicitado no read-me
-          onClick={}
+          onClick={ () => setClickBtn('drink') } // usa o click do estado para verificar o recebimento
         >
           Drinks
         </button>
       </section>
+      <ul>
+        { showCard() }
+        {/* renderiza o card mapeado e filtrado da função da linha 8 */}
+      </ul>
     </>
   );
 }
